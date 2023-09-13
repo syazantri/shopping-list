053dab7821b2ad5ed861c88476987c0e7486b126
syazantri <syazantri@gmail.com>
2023-09-13 14:13:35 +0700

    tutorial 2 selesai

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -17,5 +17,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    # path('admin/', admin.site.urls),
 ]
